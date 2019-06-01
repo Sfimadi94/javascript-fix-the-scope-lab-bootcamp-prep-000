0ed0d00dc7566049f31900b59a442659dff00729
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-01 22:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function add2(n) {
   // const two = 2
 }
 
-var funkyFunction = function() {
+var funkyFunction = function funk() {
   return function() {
     return "FUNKY!"
   }
@@ -29,4 +29,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = function()
+var theFunk = funk()
